5221fbf49ea0cfecca2bbfad4bd541243f445223
Fredrik Andersson <fredrik.andersson@aol.com>
2020-05-08 16:56:59 +0200

    Set partials and dry code

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,11 @@
 class StudentsController < ApplicationController
-
+  before_action :get_student, only: [:show, :edit, :update]
+  
   def index
     @students = Student.all
   end
 
   def show
-    @student = Student.find(params[:id])
   end 
 
   def new 
@@ -23,11 +23,9 @@ class StudentsController < ApplicationController
   end 
 
   def edit
-    @student = Student.find(params[:id])
   end
   
   def update
-    @student = Student.find(params[:id])
     if @student.update(student_params)
       flash[:notice] = "Your profile was successfully updated."
       redirect_to @student
@@ -37,6 +35,10 @@ class StudentsController < ApplicationController
   end
 
   private 
+
+  def get_student
+    @student = Student.find(params[:id])
+  end
 
   def student_params
     params.require(:student).permit(:name, :email)
