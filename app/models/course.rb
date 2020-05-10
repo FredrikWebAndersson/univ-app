c0ea3722e3637594c260562b46cd8bdac9c89508
Fredrik Andersson <fredrik.andersson@aol.com>
2020-05-10 11:37:54 +0200

    Set enroll feature and new many to many association with thte StudentCourses table

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,6 @@ class Course < ApplicationRecord
   validates :name, presence: true, length: { minimum: 5, maximum: 30 }
   validates :description, presence: true, length: { minimum: 25, maximum: 250 }
   validates :code_name, presence: true, length: { minimum: 3, maximum: 6 }
+  has_many :student_courses 
+  has_many :students, through: :student_courses
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_09_125823) do
+ActiveRecord::Schema.define(version: 2020_05_09_160917) do
 
   create_table "courses", force: :cascade do |t|
     t.string "code_name"
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "student_courses", force: :cascade do |t|
+    t.integer "student_id"
+    t.integer "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
